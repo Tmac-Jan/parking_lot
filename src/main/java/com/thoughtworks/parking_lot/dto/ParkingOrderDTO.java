bcd1b0a47d3631644b830382a198a4a94e237fb8
macmanboy <macmanboy@foxmail.com>
2019-07-20 17:24:32 +0800

    [feat]:[add dto ParkingOrderDTO ]

---
+++ src/main/java/com/thoughtworks/parking_lot/dto/ParkingOrderDTO.java
@@ -0,0 +1,84 @@
+package com.thoughtworks.parking_lot.dto;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * @title: ParkingOrderDTO
+ * @projectName: parking_lot
+ * @description: TODO
+ * @author: macmanboy@foxmail.com(Gio Zhang)
+ * @date: 2019/7/20 17:22
+ */
+public class ParkingOrderDTO {
+    private Integer id;
+
+    private String parkingLotName;
+
+    private String carNumber;
+
+    private Date createTime;//订单创建时间，默认订单创建之后 只会从“创建”到“结束”
+
+    private Date updateTime;//订单结束时间
+
+    private String status;
+
+    private Integer parkingLotId;
+
+    public Integer getParkingLotId() {
+        return parkingLotId;
+    }
+
+    public void setParkingLotId(Integer parkingLotId) {
+        this.parkingLotId = parkingLotId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getParkingLotName() {
+        return parkingLotName;
+    }
+
+    public void setParkingLotName(String parkingLotName) {
+        this.parkingLotName = parkingLotName;
+    }
+
+    public String getCarNumber() {
+        return carNumber;
+    }
+
+    public void setCarNumber(String carNumber) {
+        this.carNumber = carNumber;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
